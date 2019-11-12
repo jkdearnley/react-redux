aac5b9de79ae8e044fd4a93c0a4e9f7bff7bd88d
jkdearnley <jkdearnley@gmail.com>
2019-11-13 09:37:55 +1300

    Some stuff

---
+++ my-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import BookList from './BookList'
+import BookNote from './BookNote'
+
+const App = () => {
+    return (
+        <div className="ui container grid">
+            <div className="ui grid"></div>
+            <div classsName=""></div>
+    
+    </div>
+    )
+}
